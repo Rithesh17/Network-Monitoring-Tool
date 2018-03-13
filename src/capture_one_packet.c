63c4e34405023416033a195e0430a368f2682766
adwait1-G <adwait.gautham@gmail.com>
2018-03-13 15:41:23 +0530

    Removed many TODOs. Commented stuff

--- src/capture_one_packet.c
+++ src/capture_one_packet.c
@@ -17,28 +17,42 @@
 int main(int argc , char **argv) {
 
 
-	char 			*pkp_dev_name;				
-	char     		pkp_error_buffer[PCAP_ERRBUF_SIZE];
-	char 			pkp_str_ip_addr[13];
-	char 			pkp_str_subnet_mask[13];
-	struct in_addr 		pkp_ip_address;
-	int 			pkp_packet_count_limit = DEFAULT_PACKET_COUNT_LIMIT;			
+	char 			*pkp_dev_name;				/*The device name. eg: eno0 , eth0 , wlp3s0 , etc., */
+	char     		pkp_error_buffer[PCAP_ERRBUF_SIZE];	/*Standard error buffer */
+	char 			pkp_str_ip_addr[13];			/*IP Address in human readable form(string). eg: 10.100.12.13 */
+	char 			pkp_str_subnet_mask[13];		/*Subnet mask in human readable form(string). eg: 255.255.255.240 */
+	struct in_addr 		pkp_ip_address;				
+	int 			pkp_packet_count_limit = DEFAULT_PACKET_COUNT_LIMIT;	
 	int 			pkp_timeout_limit = 100000;			//TODO: Should change. Don't know what. Will come back.
-	const unsigned char 	*pkp_packet;
-	bpf_u_int32 		pkp_raw_ip_addr;				
-	bpf_u_int32 		pkp_raw_subnet_mask;			
-	struct pcap_pkthdr 	pkp_packet_header;
-	pcap_t 			*pkp_sniff_handle;
+	const unsigned char 	*pkp_packet;				/*This pointer points to the packet captured. */
+	bpf_u_int32 		pkp_raw_ip_addr;			/*IP Address in raw/network form. */
+	bpf_u_int32 		pkp_raw_subnet_mask;			/*Subnet mask in raw/network form */
+	struct pcap_pkthdr 	pkp_packet_header;			/*Per Packet header , Ref: pcap.h */
+	pcap_t 			*pkp_sniff_handle;			/*The sniff handle */
 	
 
+/*
+ * Finding default device . 
+ * Routine: pcap_lookupdev() 
+ */
 	pkp_dev_name = pcap_lookupdev(pkp_error_buffer);
 	if(pkp_dev_name == NULL)	
 		err_exit("Error in finding default device. \nRoutine: pcap_loopupdev()" , pkp_error_buffer);
-	
+
+
+/*
+ * Getting details of the device found . 
+ * Routine: pcap_lookupnet() 
+ */
+
 
 	if(pcap_lookupnet(pkp_dev_name , &pkp_raw_ip_addr , &pkp_raw_subnet_mask , pkp_error_buffer) == -1)
 		err_exit("Error in finding details about the device. \nRoutine: pcap_lookupnet()" , pkp_error_buffer);
 		
+/*
+ * Converting Network forms of IP Address and Subnet Mask into human-readable strings. 
+ * Routine: inet routines.
+ */
 
 	pkp_ip_address.s_addr = pkp_raw_ip_addr;
 	strcpy(pkp_str_ip_addr , inet_ntoa(pkp_ip_address));
@@ -55,23 +69,37 @@ int main(int argc , char **argv) {
 	printf("Ip Address = %s\n"   , 	pkp_str_ip_addr);
 	printf("Subnet mask = %s\n"  , 	pkp_str_subnet_mask);
 
+
+/*
+ * Opening a live sniffing session. Get a handle to manage the session. 
+ * Routine: pcap_open_live()
+ */
 	
 	pkp_sniff_handle = pcap_open_live(pkp_dev_name , BUFSIZ , pkp_packet_count_limit , pkp_timeout_limit , pkp_error_buffer);		
 	if(pkp_sniff_handle == NULL) 
 		err_exit("Error:  Unable to open a live sniffing session in the device specified. Routine: pcap_open_live()" , pkp_error_buffer);
 
+/*
+ * Checking if the default device uses/supports Ethernet headers. Or Checking if the Data-Link Protocol is the Ethernet Protocol.
+ * Routine: pcap_datalink()
+ */
 
 	if(pcap_datalink(pkp_sniff_handle) != DLT_EN10MB) 
 		err_exit("Error: Device does not support Ethernet Headers." , pkp_error_buffer);	
 
-	
+/*
+ * XXX: Checking if sniffing happens properly. This will be commented.
+ * Sniffed 1 packet .
+ * Routine: pcap_next()
+ *
 	
 	pkp_packet = pcap_next(pkp_sniff_handle , &pkp_packet_header);
 	if(pkp_packet == NULL) 
 		err_exit("Error: A single packet did not get sniffed." , pkp_error_buffer);
 
 	print_packet_len(pkp_packet , pkp_packet_header);
-	
+*/	
+
 
 	return 0;
 	
